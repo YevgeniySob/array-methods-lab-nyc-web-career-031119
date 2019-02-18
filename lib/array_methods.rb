7906d68f8cc33d47ac65be0213a9ef76ad109b69
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-18 23:45:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,13 @@
 def using_include(array, element)
-
+  if array.include?(element)
+    return true
+  else
+    return false
+  end
 end
 
 def using_sort(array)
-
+  return array.sort
 end
 
 def using_reverse(array)
